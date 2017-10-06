83e399c73590675cae69d84cddbff4ba8c4f98f3
David Macias Rosario <DavidMaciasRosario@outlook.es>
2017-10-06 20:37:00 +0200

    configuracion de la web api

---
+++ TrackMyBets.WepApi/Helpers/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackMyBets.WepApi.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+
+        public static string ConnetionString { get; set; }
+
+        public AppSettings() {
+            ConnetionString = "Data Source=DESKTOP-IKARG49;Initial Catalog=BD_TRACKMYBETS;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+    }
+}
+
---
+++ TrackMyBets.WepApi/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TrackMyBets.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using TrackMyBets.WepApi.Helpers;
+using Microsoft.Extensions.Options;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TrackMyBets.WepApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddMvc();
+
+            var connectionString = AppSettings.ConnetionString;
+            services.AddDbContext<BD_TRACKMYBETSContext>(options => options.UseSqlServer(connectionString));
+
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+
+            var appSettings = app.ApplicationServices.GetService<IOptions<AppSettings>>().Value;
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                }
+            });
+
+            app.UseMvc();
+        }
+    }
+}
